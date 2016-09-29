03790e4d47cb9d949947f00540d087ed612daf3d
thomas <thomasswan5547>
2016-09-29 14:37:05 -0700

    some more things

---
+++ home/.config/autostart/startwayland.sh
@@ -0,0 +1,4 @@
+Script started, file is typescript
+]0;fish  /home/thomas[30m(B[mWelcome to fish, the friendly interactive shell
+Type [32mhelp[30m(B[m for instructions on how to use fish
+]0;fish  /home/thomas[30m(B[m[38;5;245m⏎[0m                                                                                                  [2K[44m[30m ~ [40m(B[m[34m [Kc[31m[1mc(B[m[38;5;240mlear[30m(B[m[K
